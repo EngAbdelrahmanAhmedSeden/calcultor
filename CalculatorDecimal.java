9e8706d4a014da983a36e9ad1ffe755e054b70f9
EngAbdelrahmanAhmedSeden <154686134+EngAbdelrahmanAhmedSeden@users.noreply.github.com>
2023-12-25 01:22:16 +0200

    Update CalculatorDecimal.java

--- CalculatorDecimal.java
+++ CalculatorDecimal.java
@@ -9,38 +9,26 @@ package com.mycompany.myproject;
  * @author New 1000 Meeza
  */
 public class CalculatorDecimal {
-        public static double add(double num, int counter) {
-        double sum=0;
-        for(int i=0;i<counter;i++){
-            sum+=num;
-        }
-        return sum;
+    
+    public static double add(double num, double add) {
+        return num+add;
     }
-        public static double sub(double num, int counter) {
-        double sub=0;
-        for(int i=0;i<counter;i++){
-            sub=sub-num;
-        }
-        return sub;    
-        }
-        public static double multi(double num, int counter) {
-        double multi=1;
-        for(int i=0;i<counter;i++){
-            multi*=num;
-        }
-        return multi;
+
+    public static double sub(double num, double sub) {
+        return num-sub;
     }
-    public static double div(double num, int counter)
-    {
-        double div=1;
-        for(int i=0;i<counter;i++){
-            div/=num;
-        }
-        return div;    }
-    public static double pow(double number , double pow){
+
+    public static double multi(double multi, double num) {
+        return multi*num;
+    }
+
+    public static double div(double num, double div) {
+        return num/div;
+    }
+
+    public static double pow(double number, double pow) {
         double res = number;
-        for(int i = 1; i < pow; i++)
-        {
+        for (int i = 1; i < pow; i++) {
             res *= number;
         }
         return res;
